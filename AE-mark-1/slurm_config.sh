83eee69b8090ea78591ff799c16486de7f4cd0ed
createanArray1 <essinto@gmail.com>
2023-11-11 15:47:25 +0400

    adding diffusion to dataloader

--- AE-mark-1/slurm_config.sh
+++ AE-mark-1/slurm_config.sh
@@ -10,11 +10,11 @@
 module load miniconda/3 -q
 module load cuda/11.4 -q
 
-conda create --name check python==3.8 -y -q
+conda activate check
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 -q
 pip3 install hydra-core -q
 pip3 install numpy -q 
 pip3 install matplotlib -q
 pip3 install pillow -q
-python train.py  hydra/job_logging=disabled  1> out/out.txt 2>out/err.txt
+python train_dis.py
