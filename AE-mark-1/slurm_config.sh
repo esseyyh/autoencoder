b4f68746bb1cf230238bc372e32ecb2a3d3ec155
Essey Habteslassie <89856787+createanArray1@users.noreply.github.com>
2023-11-13 09:55:55 +0400

    Update slurm_config.sh

--- AE-mark-1/slurm_config.sh
+++ AE-mark-1/slurm_config.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=train_check
-#SBATCH --time=22:00:00
+#SBATCH --time=60:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --account=teach0013
